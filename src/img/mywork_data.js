1d0b190c49254a2141f4cfbf6747f34f65361fa3
SandeepKumar4567 <sandeepkumarp4567@gmail.com>
2024-05-21 08:28:53 +0530

    added project

---
+++ src/img/mywork_data.js
@@ -0,0 +1,27 @@
+import project1_img from "../img/project_1.png";
+import project2_img from "../img/project_2.png";
+import project3_img from "../img/project_3.png";
+
+
+const mywork_data = [
+  {
+    w_no: 1,
+    w_name: "Web design",
+    w_img: project1_img,
+    w_url: "",
+  },
+  {
+    w_no: 2,
+    w_name: "Web design",
+    w_img: project2_img,
+    w_url: "",
+  },
+  {
+    w_no: 3,
+    w_name: "Web design",
+    w_img: project3_img,
+    w_url: "",
+  },
+];
+
+export default mywork_data;
---
+++ src/img/services_data.js
@@ -0,0 +1,19 @@
+const Services_Data = [
+  {
+    s_no: "01",
+    s_name: "Full Stack Development",
+    s_desc: "From the scarch to the database",
+  },
+  {
+    s_no: "02",
+    s_name: "Web Developmet",
+    s_desc: "I do separate phases also...Front-end, Back-end, Database",
+  },
+  {
+    s_no: "03",
+    s_name: "App design",
+    s_desc: "App design is the process of building, programming...",
+  },
+];
+
+export default Services_Data;
